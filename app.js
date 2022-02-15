ea936cb9039fd3274e1fca08bdc7bbe2a39d28cd
Ivan Xie Ng (irx521) <irx521@mail.usask.ca>
2022-02-15 03:10:31 -0600

    app #2 commit

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const passport = require('passport');
+const session = require('express-session');
+const flash = require('connect-flash');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+
+
+
+
